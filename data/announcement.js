033db7830722e2d0bf5e984cb1b93293b86296ac
david7tor7harrison-hue <david7tor7harrison@gmail.com>
2026-06-05 17:48:52 +0530

    Add files via upload

---
+++ data/announcement.js
@@ -0,0 +1,24 @@
+const announcements = [
+
+{
+id:1,
+type:"important",
+message:
+"🔥 SOLO TOP PRIZE MATCH LIVE NOW • JOIN FAST"
+},
+
+{
+id:2,
+type:"update",
+message:
+"🎯 Room ID & Password Match Start Se 10 Min Pehle Share Hoga"
+},
+
+{
+id:3,
+type:"notice",
+message:
+"⚠ Recording Mandatory Hai Prize Claim Ke Liye"
+}
+
+];
